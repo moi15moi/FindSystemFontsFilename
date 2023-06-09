5e434a8651a5f56c28af3aced4ae83577f5d8214
moi15moi <moi15moismokerlolilol@gmail.com>
2023-06-08 20:48:17 -0400

    [__init__] Import AndroidLibraryNotFound exception

--- find_system_fonts_filename/__init__.py
+++ find_system_fonts_filename/__init__.py
@@ -1,4 +1,4 @@
 from .fonts_filename import get_system_fonts_filename
-from .exceptions import FontConfigNotFound, OSNotSupported
+from .exceptions import AndroidLibraryNotFound, FontConfigNotFound, OSNotSupported
 
 __version__ = "0.0.4"
