d4d9ca3434d96dc61ed9bd7b6c6dd33bccd4416e
moi15moi <moi15moismokerlolilol@gmail.com>
2023-06-08 21:03:15 -0400

    Bump version to 0.1.1

--- find_system_fonts_filename/__init__.py
+++ find_system_fonts_filename/__init__.py
@@ -1,4 +1,4 @@
 from .fonts_filename import get_system_fonts_filename
 from .exceptions import AndroidLibraryNotFound, FontConfigNotFound, OSNotSupported
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
